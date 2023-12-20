6527a471059733022466db14017f4f0346a76fd3
SimoneRimedio <rimediosimone1@gmail.com>
2023-12-20 19:18:43 +0100

    added passport config

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const loginController = require("./src/controllers/auth/login");
 const registerController = require("./src/controllers/auth/register");
@@ -8,6 +11,41 @@ const postPostitController = require("./src/controllers/postit/postPostit");
 const getPostitController = require("./src/controllers/postit/getPostit");
 
 const app = express();
+
+// Configurazione di Passport
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    const user = users.find((u) => u.username === username);
+    if (!user) {
+      return done(null, false, { message: "Utente non trovato." });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: "Password errata." });
+    }
+    return done(null, user);
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  const user = users.find((u) => u.id === id);
+  done(null, user);
+});
+
+// Sessione e inizializzazione di Passport
+app.use(
+  session({
+    secret: "il_tuo_segreto",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
